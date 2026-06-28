4483eb8d66c9b27e19f07a3e98916a51257db134
Prajwal Nayaka T <prajwalnayakat@gmail.com>
2026-06-28 16:01:02 +0530

    Update train_tox21.py

--- EGNN Training/train_tox21.py
+++ EGNN Training/train_tox21.py
@@ -72,7 +72,7 @@ def train():
 
     # Load the pre-trained QM9 model (out_features=1, as trained)
     model = MoleColyteEGNN(out_features=1)
-    torch.load("/kaggle/working/molecolyte_egnn_qm9_pretrained_best.pt", weights_only=True)
+    torch.load("/kaggle/working/molecolyte_egnn_qm9_best.pt", weights_only=True)
 
     # Swap the prediction head for Tox21's 12 binary targets
     model.prediction_head = nn.Sequential(
